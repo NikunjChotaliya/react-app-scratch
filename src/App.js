fadd38609502e74eaa0faea10534dc69cf644c2b
Nikunj Chotaliya <nikunjc@pulsesecure.net>
2021-05-08 16:22:13 +0530

    webpack version v1

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { hot } from 'react-hot-loader';
 import './app.css';
 import CircleImg from './assets/circle.png';
 import Company1Img from './assets/company1.jpg';
 import Cart from './assets/cart.svg';
 import txtFile from './assets/content.txt';
+import 'lodash';
+const Component1 = React.lazy(() => import('./Components/Component1'));
+const Component2 = React.lazy(() => import('./Components/Component2'));
 
 function App() {
 
   return (
-    <div className="app">
-      From React App 12
+    <Suspense fallback={<>Loading...</>}>
+      <div className="app">
+        From React App 12
       <img
-        src={Cart}
-        alt="SVG Alt"
-        height={200}
-        width={200}
-      />
-      <img
-        src={CircleImg}
-        alt="CircleImg Image Alt"
-        height={200}
-        width={200}
-      />
-      <img
-        src={Company1Img}
-        alt="Company1Img Image Alt"
-        height={200}
-        width={200}
-      />
-      <p>{txtFile}</p>
-    </div>
+          src={Cart}
+          alt="SVG Alt"
+          height={200}
+          width={200}
+        />
+        <img
+          src={CircleImg}
+          alt="CircleImg Image Alt"
+          height={200}
+          width={200}
+        />
+        <img
+          src={Company1Img}
+          alt="Company1Img Image Alt"
+          height={200}
+          width={200}
+        />
+        <p>{txtFile}</p>
+      </div>
+      <Component1 />
+      <Component2 />
+    </Suspense>
+
   )
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,98 @@
 let path = require('path');
 let webpack = require('webpack');
-let svgToMiniDataURI = require('mini-svg-data-uri');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const ESLintPlugin = require('eslint-webpack-plugin');
+// let svgToMiniDataURI = require('mini-svg-data-uri');
 
-module.exports = {
-  entry: './src/index.js',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: ["@babel/env"]
-        }
+module.exports = function (_, { env, ...params }) {
+  const { mode } = params;
+  return {
+    entry: {
+      main: {
+        import: './src/index.js',
+        dependOn: 'vendors'
       },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(svg|png|jpg|jpeg)$/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'pictures/[hash][ext][query]'
+      vendors: ['react', 'react-dom', 'lodash'],
+    },
+    // entry: './src/index.js',
+    mode,
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          options: {
+            presets: ["@babel/env"]
+          }
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.(svg|png|jpg|jpeg)$/,
+          type: 'asset/resource',
+          generator: {
+            filename: 'static/images/[name][hash][ext]'
+          }
+        },
+        // {
+        //   test: /\.svg/,
+        //   type: 'asset/inline',
+        //   generator: {
+        //     dataUrl: content => {
+        //       content = content.toString();
+        //       console.log(svgToMiniDataURI(content))
+        //       return svgToMiniDataURI(content);
+        //     },
+        //   }
+        // },
+        {
+          test: /\.txt/,
+          type: 'asset/source',
+          generator: {
+            filename: 'static/files/[hash][ext][query]'
+          }
         }
+      ]
+    },
+    resolve: {
+      extensions: ["*", ".js", ".jsx"],
+      alias: {
+        Components: path.resolve(__dirname, 'src/Components/')
       },
-      // {
-      //   test: /\.svg/,
-      //   type: 'asset/inline',
-      //   generator: {
-      //     dataUrl: content => {
-      //       content = content.toString();
-      //       console.log(svgToMiniDataURI(content))
-      //       return svgToMiniDataURI(content);
-      //     },
-      //   }
-      // },
-      {
-        test: /\.txt/,
-        type: 'asset/source',
-        generator: {
-          filename: 'files/[hash][ext][query]'
-        }
-      }
+      descriptionFiles: ['package.json'],
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist/'),
+      // publicPath: '/dist/',
+      // filename: '[name].bundle.js',
+      filename: (pathData) => {
+        return pathData.chunk.name === 'main' ? '[name].js' : 'static/js/[name].bundle.js';
+      },
+      chunkFilename: 'static/js/[name].[contenthash].bundle.js',
+      // assetModuleFilename: 'static/images/[hash][ext]',
+      clean: true,
+      // library: ['MyLibrary', '[name]'], // name is a placeholder here
+      // libraryTarget: 'umd',
+      library: {
+        name: ['MyLibrary', '[name]'], // name is a placeholder here
+        type: 'window',
+      },
+    },
+    devServer: {
+      contentBase: path.join(__dirname, "dist/"),
+      port: 3001,
+      hot: true
+    },
+    plugins: [
+      new webpack.ProgressPlugin(),
+      new webpack.HotModuleReplacementPlugin(),
+      new HtmlWebpackPlugin({
+        template: './public/index.html'
+      }),
+      // new ESLintPlugin({}),
     ]
-  },
-  resolve: { extensions: ["*", ".js", ".jsx"] },
-  output: {
-    path: path.resolve(__dirname, 'dist/'),
-    publicPath: '/dist/',
-    filename: 'bundle.js'
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "public/"),
-    port: 3000,
-    hot: true
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+  }
 }
